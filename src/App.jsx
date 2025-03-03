2045bd2c1eee2889b7b44eebe2d21bbd05fb1f9a
patriciakennedy <42810547+patriciakennedy@users.noreply.github.com>
2025-03-03 04:14:52 -0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import { Button } from './components/ui/button';
+
+function App() {
+    return (
+        <>
+            <div>
+                <h1 class="text-5xl">JESUS IS LORD</h1>
+                <Button>Button</Button>
+            </div>
+        </>
+    );
+}
+
+export default App;
