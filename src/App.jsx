e9a4ad57541fe6cd3d1f81a7f27e73e1c2774edd
patriciakennedy <42810547+patriciakennedy@users.noreply.github.com>
2025-03-03 10:04:25 -0600

    Update app-layout.jsx routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
 import { Button } from './components/ui/button';
+
+// Create the router that will be used to setup our project routes
+const router = createBrowserRouter([
+    {
+        element: <AppLayout />, //randers a layout that wraps all pages
+        children: [
+            {
+                path: '/',
+                element: <LandingPage />,
+            },
+        ],
+    },
+]);
 
 function App() {
     return (
         <>
-            <div>
-                <h1 class="text-5xl">JESUS IS LORD</h1>
-                <Button>Button</Button>
-            </div>
+        <RouterProvider router={router}/>
+            <h1></h1>
         </>
     );
 }
