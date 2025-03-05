bb7ce7988704c397af0065f355e15ade8837e7fb
patriciakennedy <42810547+patriciakennedy@users.noreply.github.com>
2025-03-04 19:04:19 -0600

    chore: add initial project file structure and templates

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,12 @@ import './App.css';
 import { Button } from './components/ui/button';
 import AppLayout from './layout/AppLayout';
 import LandingPage from './pages/Landing';
+import OnBoarding from './pages/OnBoarding';
+import JobListing from './pages/JobListing';
+import JobPage from './pages/JobPage';
+import MyJobs from './pages/MyJobs';
+import PostJobs from './pages/PostJobs';
+import SavedJobs from './pages/SavedJobs';
 
 // Create the router that will be used to setup our project routes
 const router = createBrowserRouter([
@@ -12,6 +18,30 @@ const router = createBrowserRouter([
             {
                 path: '/',
                 element: <LandingPage />,
+            },
+            {
+                path: '/OnBoarding',
+                element: <OnBoarding />,
+            },
+            {
+                path: '/JobListing',
+                element: <JobListing />,
+            },
+            {
+                path: '/JobPage/:id',
+                element: <JobPage />,
+            },
+            {
+                path: '/MyJobs',
+                element: <MyJobs />,
+            },
+            {
+                path: '/PostJobs',
+                element: <PostJobs />,
+            },
+            {
+                path: '/SavedJobs',
+                element: <SavedJobs />,
             },
         ],
     },
---
+++ src/pages/PostJobs.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const PostJobs = () => {
+    return <div>PostJobs</div>;
+};
+
+export default PostJobs;
