0f1145cd9859e8879ba9d42a06cb3e87be81ed8d
patriciakennedy <42810547+patriciakennedy@users.noreply.github.com>
2025-03-04 20:05:03 -0600

    feat(layout): add Header component with initial content

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -1,12 +1,37 @@
-import React from 'react';
+import Header from '../components/Header';
 import { Outlet } from 'react-router-dom';
 
 const AppLayout = () => {
     return (
-        <div className="grid-background">
-            <Outlet />
+        <div>
+            <div className="grid-background"></div>
+            <main className="min-h-screen container">
+                <Header />
+                <Outlet />
+            </main>
+            <div className="p-10 text-center bg-gray-800 mt-10">
+                Made with 💗 by RoadsideCoder
+            </div>
         </div>
     );
 };
 
 export default AppLayout;
+
+// import React from 'react';
+// import { Outlet } from 'react-router-dom';
+// import Header from '../components/Header';
+
+// const AppLayout = () => {
+//     return (
+//         <div className="grid-background">
+//             <main className="min-h-screen">
+//                 <Header />
+//                 <Outlet />
+//             </main>
+//             <div className="p-10 text-center bg-grey-800 mt-10">Made with love by Pat Kennedy</div>
+//         </div>
+//     );
+// };
+
+// export default AppLayout;
