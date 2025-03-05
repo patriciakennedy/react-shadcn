c1e9f518efcb1a0d79da2ce4e1c9ed564efcfd90
patriciakennedy <42810547+patriciakennedy@users.noreply.github.com>
2025-03-04 18:09:26 -0600

    feat(layout): add <Outlet /> to render child routes inside AppLayout

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
 
 const AppLayout = () => {
-    return <div>App Layout</div>;
+    return (
+        <div>
+            App Layout
+            <Outlet />
+        </div>
+    );
 };
 
 export default AppLayout;
