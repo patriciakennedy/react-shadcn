0fac5b3854d05c5823437f4777711e3dd4708060
patriciakennedy <42810547+patriciakennedy@users.noreply.github.com>
2025-03-05 10:57:22 -0600

    Update AppLayout.jsx

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -10,28 +10,10 @@ const AppLayout = () => {
                 <Outlet />
             </main>
             <div className="p-10 text-center bg-gray-800 mt-10">
-                Made with 💗 by RoadsideCoder
+                Made with 💗 by Patricia Kennedy
             </div>
         </div>
     );
 };
 
 export default AppLayout;
-
-// import React from 'react';
-// import { Outlet } from 'react-router-dom';
-// import Header from '../components/Header';
-
-// const AppLayout = () => {
-//     return (
-//         <div className="grid-background">
-//             <main className="min-h-screen">
-//                 <Header />
-//                 <Outlet />
-//             </main>
-//             <div className="p-10 text-center bg-grey-800 mt-10">Made with love by Pat Kennedy</div>
-//         </div>
-//     );
-// };
-
-// export default AppLayout;
