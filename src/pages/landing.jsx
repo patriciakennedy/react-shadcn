1ac4611ed6c9b346f7106a55bf6e42a76c29a708
patriciakennedy <42810547+patriciakennedy@users.noreply.github.com>
2025-03-07 18:08:17 -0600

    Add company logo carousel to the landing page for enhanced UI. Improve layout and responsiveness.

--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -1,13 +1,21 @@
 import { Button } from '../components/ui/button';
 import { Link } from 'react-router-dom';
 // import { Button } from '../components/ui/button';
+import Autoplay from 'embla-carousel-autoplay';
+import companies from '../data/companies.json';
+
+import {
+    Carousel,
+    CarouselContent,
+    CarouselItem,
+} from '../components/ui/carousel';
 
 const LandingPage = () => {
     return (
         <main className="flex flex-col gap-10 sm:gap-20 py-10 sm:py-20">
             <section className="text-center ">
                 <h1 className="flex flex-col items-center justify-center gradient-title gradient-title font-extrabold text-4xl sm:text-6xl lg:text-8xl tracking-tighter py-4">
-                    Find You Dream Job{' '}
+                    Find Your Dream Job{' '}
                     <span className="flex items-center gap-2 sm:gap-6">
                         and get{' '}
                         <img
@@ -37,6 +45,34 @@ const LandingPage = () => {
             </div>
 
             {/* carousel */}
+            <Carousel
+                plugins={[
+                    Autoplay({
+                        delay: 2000,
+                    }),
+                ]}
+                plugins={[
+                    Autoplay({
+                        delay: 2000,
+                    }),
+                ]}
+                className="w-full py-10"
+            >
+                <CarouselContent className="flex gap-5 sm:gap-20 items-center">
+                    {companies.map(({ name, id, path }) => (
+                        <CarouselItem
+                            key={id}
+                            className="basis-1/3 lg:basis-1/6"
+                        >
+                            <img
+                                src={path}
+                                alt={name}
+                                className="h-9 sm:h-14 object-contain"
+                            />
+                        </CarouselItem>
+                    ))}
+                </CarouselContent>
+            </Carousel>
             {/* banner */}
 
             <section>{/* cards */}</section>
